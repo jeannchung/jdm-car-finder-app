ca816929964166c4a6b3dd4aa88f92d8d74d44eb
jean.chung <jeannchung@gmail.com>
2018-10-31 17:58:00 -0700

    final commit

---
+++ data/carArray.js
@@ -0,0 +1,65 @@
+// cars to match with here
+let carArray = [
+  {
+    id: 1,
+    name: 'Subaru WRX STI',
+    price: '$36,595',
+    age: '2018', 
+    make: 'Import', 
+    body: 'Aggressive',
+    fuel: '19 mpg',
+    trans: 'Manual',
+    drive: 'All Wheel Drive',
+    img_url: '../public/assets/images/sti.jpg'
+  },
+  {
+    id: 2,
+    name: 'Mitsubishi Evolution X',
+    price: '$37,995',
+    age: '2016', 
+    make: 'Import', 
+    body: 'Standard/Moderate',
+    fuel: '19 mpg',
+    trans: 'Manual',
+    drive: 'All Wheel Drive',
+    img_url: '../public/assets/images/evo.jpg'
+  },
+  {
+    id: 3,
+    name: 'Ford Focus RS',
+    price: '$41,120',
+    age: '2018', 
+    make: 'Domestic', 
+    body: 'Aggressive',
+    fuel: '22 mpg',
+    trans: 'Manual',
+    drive: 'All Wheel Drive',
+    img_url: '../public/assets/images/rs.jpg'
+  },
+  {
+    id: 4,
+    name: 'Honda Civic Type R',
+    price: '$34,700',
+    age: '2018', 
+    make: 'Import', 
+    body: 'Aggressive',
+    fuel: '25 mpg',
+    trans: 'Manual',
+    drive: 'Front Wheel Drive',
+    img_url: '../public/assets/images/typer.jpg'
+  },
+  {
+    id: 5,
+    name: 'Hyundai Veloster N',
+    price: '$30,000',
+    age: '2019', 
+    make: 'Import', 
+    body: 'Aggressive',
+    fuel: '25 mpg',
+    trans: 'Manual',
+    drive: 'Front Wheel Drive',
+    img_url: '../public/assets/images/n.jpg'
+  },
+]
+
+module.exports = carArray
